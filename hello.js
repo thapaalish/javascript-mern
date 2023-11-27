ae225fdc8c9611a09d421715505bf169e3dde84f
thapaalish <thapaalish44@gmail.com>
2023-11-27 20:55:05 +0545

    first commit

---
+++ hello.js
@@ -0,0 +1,31 @@
+console.log("Hello world");
+console.log("Hellooooo world");
+console.log("Helloooooooooooooooo world");
+console.log(2 + 3);
+console.log(3 * 4);
+
+// variable
+//constant
+// syntax
+// let variablename = "value"
+//keyword = reserved word
+//let = declare variable
+// const = declare constant
+//for ,if else ,switch, break
+
+// $ and _ are allowed
+// camelCase= myBottle, newUser , hasPassword
+
+// constant
+
+const PI = 3.145;
+console.log(PI);
+
+let name = "Ram";
+console.log(name);
+
+let studentName = "Alish thapa";
+console.log(studentName);
+
+const x = 2;
+console.log(x);
